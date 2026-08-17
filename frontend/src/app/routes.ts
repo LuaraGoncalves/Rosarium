00327f63d1166682fc02bbb02e8a18e394a4ab9b
Luara Gonçalves <luara.goncalvesrocha@outlook.com>
2026-08-16 23:20:52 -0300

    fix: abrir paginas no topo

--- frontend/src/app/routes.ts
+++ frontend/src/app/routes.ts
@@ -1,4 +1,5 @@
-import { createBrowserRouter } from 'react-router';
+import { Fragment, createElement, useEffect } from 'react';
+import { createBrowserRouter, Outlet, useLocation } from 'react-router';
 import { homeRoutes } from '../features/home/routes';
 import { authRoutes } from '../features/auth/routes';
 import { rosarioRoutes } from '../features/rosario/routes';
@@ -9,14 +10,38 @@ import { liturgiaRoutes } from '../features/liturgia/routes';
 import { santosRoutes } from '../features/santos/routes';
 import { errorRoutes } from '../features/erros/routes';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname]);
+
+  return null;
+}
+
+function RootRoute() {
+  return createElement(
+    Fragment,
+    null,
+    createElement(ScrollToTop),
+    createElement(Outlet)
+  );
+}
+
 export const router = createBrowserRouter([
-  ...homeRoutes,
-  ...authRoutes,
-  ...rosarioRoutes,
-  ...novenasRoutes,
-  ...oracoesRoutes,
-  ...breviarioRoutes,
-  ...liturgiaRoutes,
-  ...santosRoutes,
-  ...errorRoutes,
+  {
+    Component: RootRoute,
+    children: [
+      ...homeRoutes,
+      ...authRoutes,
+      ...rosarioRoutes,
+      ...novenasRoutes,
+      ...oracoesRoutes,
+      ...breviarioRoutes,
+      ...liturgiaRoutes,
+      ...santosRoutes,
+      ...errorRoutes,
+    ],
+  },
 ]);
